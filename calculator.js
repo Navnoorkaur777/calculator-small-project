09c7d91cd0d41abf78203367a80232e0ddf4b73e
navnoor kaur <78698497+Navnoorkaur777@users.noreply.github.com>
2022-11-16 22:59:31 +0530

    Add files via upload

---
+++ calculator.js
@@ -0,0 +1,28 @@
+
+
+
+let btns = document.querySelectorAll('button');
+let input = document.querySelector('input');
+
+let str = '';
+
+for (let btn of btns) {
+  btn.addEventListener('click', function() {
+    let btnText = btn.innerText;
+
+    if (btnText === 'AC') {
+      str = '';
+      input.value = '';
+    } else if (btnText === '=') {
+      try {
+        input.value = eval(str);
+      } catch {
+        input.value = 'Invalid'
+      }
+      str += '';
+    } else {
+      str += btnText;
+      input.value = str;
+    }
+  })
+}
